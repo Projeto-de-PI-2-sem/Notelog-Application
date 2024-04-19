f95fe053710f40c9b3a2b67517ec5ae79a5314aa
Gabrielle Śimões <gabrielle.goncalves@sptech.school>
2024-04-19 20:45:57 -0300

    subindo versao provisoria do entregavel jar em grupo

---
+++ app-note-log/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.discos.Disco;
+import com.github.britooo.looca.api.group.discos.DiscoGrupo;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Looca looca = new Looca();
+
+        System.out.println("Informações do sistema: ");
+        System.out.println(looca.getSistema());
+
+        System.out.println();
+
+        //Criação do gerenciador
+        DiscoGrupo grupoDeDiscos = looca.getGrupoDeDiscos();
+
+        //Obtendo lista de discos a partir do getter
+        List<Disco> discos = grupoDeDiscos.getDiscos();
+
+        System.out.println("Exibindo as informações de cada disco: ");
+        for (Disco disco : discos) {
+            System.out.println(disco);
+        }
+    }
+}
